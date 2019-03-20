4738a42eb739da4a86bd022a017224551fcf238c
Andrea Yoshida <aryoshida9340@gmail.com>
2019-03-19 23:25:54 -0600

    Finished it!

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,24 +15,32 @@ $(document).ready(function(){
                 var results = response.data;
                 console.log(results);
                 for (var i = 0; i < results.length; i++){
-                    if (results[i].rating !== "r" && results[i].rating !== "pg-13"){
+                    if (results[i].rating !== "r"){
                         var gifDiv = $("<div>");
                         var rating = results[i].rating;
-                        var p = $("<p>").text("Ratting: " + rating);
-                        var fruitImage = $("<img>");
-                        fruitImage.attr("src", results[i].images.fixed_height.url);
+                        var p = $("<h3>").text("Rating: " + rating);
+                        var fruitImage = $("<img data-state='still'>");
+
+                        fruitImage.attr("data-animated-url",  results[i].images.fixed_height.url);
+                        fruitImage.attr("data-still-url",  results[i].images.fixed_height_still.url);
+                        fruitImage.attr("src", results[i].images.fixed_height_still.url);
                         gifDiv.append(p);
                         gifDiv.append(fruitImage);
                         $("#gifs-go-here").prepend(gifDiv);
+
+                        fruitImage.on("click",function(){
+                            if ($(this).attr("data-state") === "still"){
+                                $(this).attr("data-state", "animated");
+                                $(this).attr("src", $(this).attr("data-animated-url"));
+                            } else {
+                                $(this).attr("data-state", "still");
+                                $(this).attr("src", $(this).attr("data-still-url"));
+                            }
+                        });
+
                     }
                 }
-
             });
         });
-
-
     });
-
-
-
 });
